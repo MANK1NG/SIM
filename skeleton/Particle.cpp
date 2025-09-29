e92e8023c454d0a9e1df90d7e33bdf8866f33a8d
Ivan Palomino <ivan.ipr.03@gmail.com>
2025-09-29 19:42:04 +0200

    Apartado 2 practica 1.1

--- skeleton/Particle.cpp
+++ skeleton/Particle.cpp
@@ -1,6 +1,6 @@
 #include "Particle.h"
 
-Particle::Particle(Vector3D Pos, Vector3D Vel) : vel(Vel)
+Particle::Particle(Vector3D Pos, Vector3D Vel, Vector3D Acc) : vel(Vel), acc(Acc)
 {
 	pose =  physx::PxTransform(physx::PxVec3(Pos.getX(), Pos.getY(), Pos.getZ()));
 	renderItem = new RenderItem(CreateShape(physx::PxSphereGeometry(2)), &pose, Vector4(0, 1, 0, 1));
@@ -14,6 +14,8 @@ Particle::~Particle()
 
 void Particle::integrate(double t)
 {
+	vel = vel + acc * t;
+
 	pose.p.x += vel.getX() * t;
 	pose.p.y += vel.getY() * t;
 	pose.p.z += vel.getZ() * t;
--- skeleton/Particle.h
+++ skeleton/Particle.h
@@ -4,12 +4,13 @@
 class Particle 
 {
 public:
-	Particle(Vector3D Pos, Vector3D Vel);
+	Particle(Vector3D Pos, Vector3D Vel, Vector3D Acc);
 	~Particle();
 
 	void integrate(double t);
 private:
 	Vector3D vel;
+	Vector3D acc;
 	physx::PxTransform pose;
 	RenderItem* renderItem;
 
